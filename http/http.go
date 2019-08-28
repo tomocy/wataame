85f90522a5dba211f5ab6a2948abdff5e7669944
tomocy <tomocy.dev@gmail.com>
2019-08-29 03:10:52 +0900

    add (Address).Compensate
    to compensate and return new address with the stored one

--- http/http.go
+++ http/http.go
@@ -1,11 +1,40 @@
 package http
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Address string
+
+func (a Address) Compensate() (string, error) {
+	host, port, err := a.split()
+	if err != nil {
+		return "", err
+	}
+	if port == "" {
+		port = "80"
+	}
+
+	return fmt.Sprintf("%s:%s", host, port), nil
+}
+
+func (a Address) split() (string, string, error) {
+	splited := strings.Split(string(a), ":")
+	if len(splited) > 2 {
+		return "", "", errors.New("invalid format of uri: the format should be host[:port]")
+	}
+	var host, port string
+	host = splited[0]
+	if len(splited) == 2 {
+		port = splited[1]
+	}
+
+	return host, port, nil
+}
 
 type Dir string
 
