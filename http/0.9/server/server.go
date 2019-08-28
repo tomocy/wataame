002abeab8f42f5ad07c6793d5fe5fc3a84e7031e
tomocy <tomocy.dev@gmail.com>
2019-08-29 02:17:59 +0900

    add Handler
    to represent a handler which recives a HTTP0.9 request and write a HTTP0.9 response to the given Writer

--- http/0.9/server/server.go
+++ http/0.9/server/server.go
@@ -1,5 +1,15 @@
 package server
 
+import (
+	"io"
+
+	http "github.com/tomocy/wataame/http/0.9"
+)
+
 type Server struct {
 	Addr string
+}
+
+type Handler interface {
+	Handle(io.Writer, *http.Request)
 }
