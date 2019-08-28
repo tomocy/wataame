a3bc43a3898ae865591cd3683251508bedb2c8a1
tomocy <tomocy.dev@gmail.com>
2019-08-29 02:16:12 +0900

    add (Dir).Open
    to open File of the given name

--- http/http.go
+++ http/http.go
@@ -1,3 +1,13 @@
 package http
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type Dir string
+
+func (d Dir) Open(name string) (*os.File, error) {
+	fname := filepath.Join(string(d), name)
+	return os.Open(fname)
+}
