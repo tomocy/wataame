e5bce8d6d571db6aeb28e2ed39cd04a38894e097
tomocy <tomocy.dev@gmail.com>
2019-08-28 23:26:35 +0900

    add (*Client).dial
    to dial to the given address on the given network with the stored Dialer

--- http/0.9/client/client.go
+++ http/0.9/client/client.go
@@ -1,9 +1,20 @@
 package client
 
-import "net"
+import (
+	"net"
+)
 
 type Client struct {
 	Dialer Dialer
+}
+
+func (c *Client) dial(network, addr string) (net.Conn, error) {
+	d := c.Dialer
+	if d == nil {
+		d = new(net.Dialer)
+	}
+
+	return d.Dial(network, addr)
 }
 
 type Dialer interface {
