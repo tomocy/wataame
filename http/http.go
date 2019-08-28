15b06a6d91225b54366626e039f6da77914e6404
tomocy <tomocy.dev@gmail.com>
2019-08-29 02:26:06 +0900

    add FileSystem
    to represent a file system

--- http/http.go
+++ http/http.go
@@ -11,3 +11,7 @@ func (d Dir) Open(name string) (*os.File, error) {
 	fname := filepath.Join(string(d), name)
 	return os.Open(fname)
 }
+
+type FileSystem interface {
+	Open(string) (*os.File, error)
+}
