4a6fe24023d2eb129eb135093d5e07ce206f18f9
tomocy <tomocy.dev@gmail.com>
2019-08-28 23:27:26 +0900

    add (*Client).dialForRequest
    to dial with the given Request

--- http/0.9/client/client.go
+++ http/0.9/client/client.go
@@ -2,10 +2,21 @@ package client
 
 import (
 	"net"
+
+	http "github.com/tomocy/wataame/http/0.9"
 )
 
 type Client struct {
 	Dialer Dialer
+}
+
+func (c *Client) dialForRequest(r *http.Request) (net.Conn, error) {
+	addr, err := r.Address()
+	if err != nil {
+		return nil, err
+	}
+
+	return c.dial("tcp", addr)
 }
 
 func (c *Client) dial(network, addr string) (net.Conn, error) {
