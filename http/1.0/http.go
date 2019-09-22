743a24d8a0f5145c8d536e4e4a0ef5f40fbaece5
tomocy <tomocy.dev@gmail.com>
2019-09-22 19:52:07 +0900

    add Methods
    to represent HTTP1.0 GET, HTTP1.0 HEAD, and HTTP1.0 POST

--- http/1.0/http.go
+++ http/1.0/http.go
@@ -3,6 +3,14 @@ package http
 import (
 	"io"
 	"net/url"
+
+	http0_9 "github.com/tomocy/wataame/http/0.9"
+)
+
+const (
+	MethodGet  = http0_9.MethodGet
+	MethodHead = "HEAD"
+	MethodPost = "POST"
 )
 
 type Request struct {
