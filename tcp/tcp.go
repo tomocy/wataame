875c7ee7d10a70352a2fa3ed9ad206c1c1b8e142
tomocy <tomocy.dev@gmail.com>
2019-09-30 19:44:21 +0900

    add (*GoDialer).Dial
    for GoDialer to dial to the given address with the given context

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -13,6 +13,10 @@ type GoDialer struct {
 	net.Dialer
 }
 
+func (d *GoDialer) Dial(ctx context.Context, addr string) (Conn, error) {
+	return d.Dialer.DialContext(ctx, "tcp", addr)
+}
+
 type Conn interface {
 	net.Conn
 }
