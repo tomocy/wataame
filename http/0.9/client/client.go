0c1617470041b0dfcb5fa94c5708ddbb4b699a97
tomocy <tomocy.dev@gmail.com>
2019-09-30 19:37:02 +0900

    fix

--- http/0.9/client/client.go
+++ http/0.9/client/client.go
@@ -33,6 +33,24 @@ func (c *Client) Do(ctx context.Context, r *http0_9.Request) (http0_9.Response, 
 	}
 }
 
+func (c *Client) dialForRequest(ctx context.Context, r *http0_9.Request) (net.Conn, error) {
+	addr, err := http.Addr(r.URI.Host).Compensate()
+	if err != nil {
+		return nil, err
+	}
+
+	return c.dial(ctx, "tcp", addr)
+}
+
+func (c *Client) dial(ctx context.Context, network, addr string) (net.Conn, error) {
+	d := c.Dialer
+	if d == nil {
+		d = new(DefaultDialer)
+	}
+
+	return d.Dial(ctx, network, addr)
+}
+
 func (c *Client) receive(conn net.Conn) (<-chan http0_9.Response, <-chan error) {
 	respCh, errCh := make(chan http0_9.Response), make(chan error)
 	go func() {
@@ -51,24 +69,6 @@ func (c *Client) receive(conn net.Conn) (<-chan http0_9.Response, <-chan error) 
 	}()
 
 	return respCh, errCh
-}
-
-func (c *Client) dialForRequest(ctx context.Context, r *http0_9.Request) (net.Conn, error) {
-	addr, err := http.Addr(r.URI.Host).Compensate()
-	if err != nil {
-		return nil, err
-	}
-
-	return c.dial(ctx, "tcp", addr)
-}
-
-func (c *Client) dial(ctx context.Context, network, addr string) (net.Conn, error) {
-	d := c.Dialer
-	if d == nil {
-		d = new(DefaultDialer)
-	}
-
-	return d.Dial(ctx, network, addr)
 }
 
 type Dialer interface {
