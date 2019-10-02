23e2766538b602dd05ac00a5aef7351c6588c778
tomocy <tomocy.dev@gmail.com>
2019-10-02 20:52:06 +0900

    add (*Request).ReadFrom
    for Request to read raw HTTP0.9 request from the given Reader

--- http/0.9/http.go
+++ http/0.9/http.go
@@ -24,6 +24,11 @@ func (r *Request) String() string {
 	return fmt.Sprintf("%s %s", r.Method, r.URI.Path)
 }
 
+func (r *Request) ReadFrom(src io.Reader) (int64, error) {
+	n, err := fmt.Fscan(src, r)
+	return int64(n), err
+}
+
 func (r *Request) Scan(state fmt.ScanState, _ rune) error {
 	var uri http.ScannableURL
 	if _, err := fmt.Fscanf(state, "%s %v", &r.Method, &uri); err != nil {
--- http/0.9/http_test.go
+++ http/0.9/http_test.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"fmt"
 	"net/url"
 	"strings"
 	"testing"
@@ -12,11 +13,39 @@ func TestRequest_WriteTo(t *testing.T) {
 		Method: MethodGet, URI: uri,
 	}
 	expected := "GET /index.html\n"
+
 	var b strings.Builder
 	subject.WriteTo(&b)
 	actual := b.String()
-
 	if actual != expected {
 		t.Errorf("unexpected (*Request).WriteTo: got %s, expect %s\n", actual, expected)
 	}
+}
+
+func TestRequest_ReadFrom(t *testing.T) {
+	uri, _ := url.Parse("http:///index.html")
+	input := "GET /index.html"
+	expected := &Request{
+		Method: MethodGet, URI: uri,
+	}
+
+	actual := new(Request)
+	if _, err := actual.ReadFrom(strings.NewReader(input)); err != nil {
+		t.Errorf("unexpected error from (*Request).ReadFrom: got %s, expect nil\n", err)
+		return
+	}
+	if err := assertRequest(actual, expected); err != nil {
+		t.Errorf("unexpected (*Request).ReadFrom: %s\n", err)
+	}
+}
+
+func assertRequest(actual, expected *Request) error {
+	if actual.Method != expected.Method {
+		return fmt.Errorf("unexpected method of request: got %s, expect %s", actual.Method, expected.Method)
+	}
+	if actual.URI.String() != expected.URI.String() {
+		return fmt.Errorf("unexpected uri of request: got %s, expect %s", actual.URI, expected.URI)
+	}
+
+	return nil
 }
