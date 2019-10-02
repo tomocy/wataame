fe39ea03fe91c9ce71eb14518f35f1436b966d90
tomocy <tomocy.dev@gmail.com>
2019-10-02 20:35:35 +0900

    add (*ScannableURL).Scan
    for ScannableURL to scan raw uri from the given ScanState and store it

--- http/http.go
+++ http/http.go
@@ -1,9 +1,11 @@
 package http
 
 import (
+	"fmt"
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Dir string
@@ -18,3 +20,22 @@ type FileSystem interface {
 }
 
 type ScannableURL url.URL
+
+func (u *ScannableURL) Scan(state fmt.ScanState, _ rune) error {
+	var raw string
+	if _, err := fmt.Fscan(state, &raw); err != nil {
+		return fmt.Errorf("failed to scan uri: %s", err)
+	}
+	if !strings.HasPrefix(raw, "http://") && !strings.HasPrefix(raw, "https://") {
+		raw = "http://" + raw
+	}
+
+	parsed, err := url.Parse(raw)
+	if err != nil {
+		return fmt.Errorf("failed to scan uri: %s", err)
+	}
+
+	*u = ScannableURL(*parsed)
+
+	return nil
+}
