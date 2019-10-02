ba3473e6c6cce7ff76d2c7243b7f83423cabda2b
tomocy <tomocy.dev@gmail.com>
2019-10-02 20:42:49 +0900

    add (*Request).Scan
    for Request to scan raw request from the given StateScan and store it

--- http/0.9/http.go
+++ http/0.9/http.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"net/url"
+
+	"github.com/tomocy/wataame/http"
 )
 
 const MethodGet = "GET"
@@ -20,6 +22,17 @@ func (r *Request) WriteTo(dst io.Writer) (int64, error) {
 
 func (r *Request) String() string {
 	return fmt.Sprintf("%s %s", r.Method, r.URI.Path)
+}
+
+func (r *Request) Scan(state fmt.ScanState, _ rune) error {
+	var uri http.ScannableURL
+	if _, err := fmt.Fscanf(state, "%s %v", &r.Method, &uri); err != nil {
+		return fmt.Errorf("failed to scan request: %s", err)
+	}
+	casted := url.URL(uri)
+	r.URI = &casted
+
+	return nil
 }
 
 type Response []byte
