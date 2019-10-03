5199da6ad7ca6ae5d65d786de9cf584856f6f21e
tomocy <tomocy.dev@gmail.com>
2019-10-03 19:38:18 +0900

    add (*Client).Do
    for HTTP1.0 client to do the given request with the given context

--- http/1.0/client/client.go
+++ http/1.0/client/client.go
@@ -1,7 +1,20 @@
 package client
 
-import "github.com/tomocy/wataame/tcp"
+import (
+	"context"
+	"fmt"
+
+	http1_0 "github.com/tomocy/wataame/http/1.0"
+	"github.com/tomocy/wataame/tcp"
+)
 
 type Client struct {
 	Dialer tcp.Dialer
+}
+
+func (c *Client) Do(ctx context.Context, r http1_0.Request) (http1_0.Response, error) {
+	switch r.(type) {
+	default:
+		panic(fmt.Sprintf("failed to do: unsupported request type: %T", r))
+	}
 }
