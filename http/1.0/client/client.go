e2a4fcdb44d18cb9f05746e0c8812668c6fd2590
tomocy <tomocy.dev@gmail.com>
2019-10-04 17:26:25 +0900

    update (*Client).Do
    for Client to do simple requsest when the given request is SimpleRequest

--- http/1.0/client/client.go
+++ http/1.0/client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/tomocy/wataame/http/0.9/client"
 	http1_0 "github.com/tomocy/wataame/http/1.0"
 	"github.com/tomocy/wataame/tcp"
 )
@@ -13,8 +14,28 @@ type Client struct {
 }
 
 func (c *Client) Do(ctx context.Context, r http1_0.Request) (http1_0.Response, error) {
-	switch r.(type) {
+	switch r := r.(type) {
+	case *http1_0.SimpleRequest:
+		resp, err := c.doSimpleRequest(ctx, r)
+		if err != nil {
+			return nil, fmt.Errorf("failed to do: %s", err)
+		}
+		return resp, nil
 	default:
 		panic(fmt.Sprintf("failed to do: unsupported request type: %T", r))
 	}
+}
+
+func (c *Client) doSimpleRequest(ctx context.Context, r *http1_0.SimpleRequest) (*http1_0.SimpleResponse, error) {
+	delegated := &client.Client{
+		Dialer: c.Dialer,
+	}
+	resp, err := delegated.Do(ctx, &r.Request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to do simple request: %s", err)
+	}
+
+	return &http1_0.SimpleResponse{
+		Response: resp,
+	}, nil
 }
---
+++ http/1.0/client/client_test.go
@@ -0,0 +1,56 @@
+package client
+
+import (
+	"context"
+	"io"
+	"net/url"
+	"testing"
+
+	http0_9 "github.com/tomocy/wataame/http/0.9"
+	"github.com/tomocy/wataame/http/0.9/server"
+	http1_0 "github.com/tomocy/wataame/http/1.0"
+)
+
+func TestClient_Do(t *testing.T) {
+	testers := map[string]func(t *testing.T){
+		"simple request": testClient_DoSimpleRequest,
+	}
+
+	for name, tester := range testers {
+		t.Run(name, tester)
+	}
+}
+
+func testClient_DoSimpleRequest(t *testing.T) {
+	addr := ":1234"
+	uri, _ := url.Parse("http://localhost" + addr + "/index.html")
+	serv := &server.Server{
+		Addr: addr,
+		Handler: server.HandlerFunc(func(w io.Writer, r *http0_9.Request) {
+			r.WriteTo(w)
+		}),
+	}
+	go func() {
+		serv.ListenAndServe()
+	}()
+	var client Client
+
+	input := &http1_0.SimpleRequest{
+		Request: http0_9.Request{
+			Method: http1_0.MethodGet, URI: uri,
+		},
+	}
+	expected := "GET /index.html\n"
+
+	resp, err := client.Do(context.Background(), input)
+	if err != nil {
+		t.Fatalf("unexpected error from (*Client).Do: got %s, expect nil\n", err)
+	}
+	actual, ok := resp.(*http1_0.SimpleResponse)
+	if !ok {
+		t.Fatalf("unexpected type of Response from (*Clinet): got %T, expect *SimpleResponse\n", resp)
+	}
+	if string(actual.Response) != expected {
+		t.Errorf("unexpected Response from (*Client).Do: got %s, expect %s\n", string(actual.Response), expected)
+	}
+}
