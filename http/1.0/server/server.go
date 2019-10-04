0eef3362bae035a467392a59aac31c9ab32b0bb3
tomocy <tomocy.dev@gmail.com>
2019-10-04 19:48:19 +0900

    add SimpleHandler
    to represent a handler which handles HTTP1.0 simple request

--- http/1.0/server/server.go
+++ http/1.0/server/server.go
@@ -1,5 +1,11 @@
 package server
 
+import http1_0 "github.com/tomocy/wataame/http/1.0"
+
 type Server struct {
 	Addr string
+}
+
+type SimpleHandler interface {
+	Handle(*http1_0.SimpleResponse, *http1_0.SimpleRequest)
 }
