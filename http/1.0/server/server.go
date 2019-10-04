d62bd55fe3e1b5128d56a104ba29f66b66e6f82a
tomocy <tomocy.dev@gmail.com>
2019-10-04 21:10:40 +0900

    add (*Server).ListenAndServe
    for Server to listen and serve HTTP1.0 requests

--- http/1.0/server/server.go
+++ http/1.0/server/server.go
@@ -1,10 +1,74 @@
 package server
 
-import http1_0 "github.com/tomocy/wataame/http/1.0"
+import (
+	"fmt"
+	"net"
+
+	"github.com/tomocy/wataame/http"
+	http1_0 "github.com/tomocy/wataame/http/1.0"
+	"github.com/tomocy/wataame/ip"
+)
 
 type Server struct {
 	Addr          string
 	SimpleHandler SimpleHandler
+}
+
+func (s *Server) ListenAndServe() error {
+	l, err := s.listen()
+	if err != nil {
+		return fmt.Errorf("failed to listen and serve: %s", err)
+	}
+	defer l.Close()
+
+	if err := s.Serve(l); err != nil {
+		return fmt.Errorf("failed to listen and serve: %s", err)
+	}
+
+	return nil
+}
+
+func (s *Server) listen() (net.Listener, error) {
+	compensated, err := ip.Addr(s.Addr).Compensate()
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen: %s", err)
+	}
+
+	l, err := net.Listen("tcp", compensated)
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen: %s", err)
+	}
+
+	return l, nil
+}
+
+func (s *Server) Serve(l net.Listener) error {
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return fmt.Errorf("failed to serve: %s", err)
+		}
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+	peekable := &http.PeekableConn{
+		Conn: conn,
+	}
+	v, err := http.DetectVersion(peekable)
+
+	if err != nil {
+		fmt.Fprintf(conn, "failed to handle: %s\n", err)
+		return
+	}
+
+	switch v {
+	default:
+		fmt.Fprintf(conn, "failed to handle: unsupported HTTP version: %s\n", v)
+		return
+	}
 }
 
 type SimpleHandler interface {
