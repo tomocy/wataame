bae91081faca7f4e849f89223221b9bce40af58a
tomocy <tomocy.dev@gmail.com>
2019-10-04 19:46:30 +0900

    delete Handler

--- http/1.0/server/server.go
+++ http/1.0/server/server.go
@@ -1,22 +1,5 @@
 package server
 
-import (
-	"io"
-
-	http1_0 "github.com/tomocy/wataame/http/1.0"
-)
-
 type Server struct {
-	Addr    string
-	Handler Handler
-}
-
-type Handler interface {
-	Handle(io.Writer, http1_0.Request)
-}
-
-type HandlerFunc func(io.Writer, http1_0.Request)
-
-func (f HandlerFunc) Handle(w io.Writer, r http1_0.Request) {
-	f(w, r)
+	Addr string
 }
