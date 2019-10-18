a55d9ace61a77753f79c167f2290f9ef4c304992
tomocy <tomocy.dev@gmail.com>
2019-10-18 23:36:52 +0900

    update (*Client).Do
    for Clien to do HTTP full requests

--- http/1.0/client/client.go
+++ http/1.0/client/client.go
@@ -3,9 +3,11 @@ package client
 import (
 	"context"
 	"fmt"
+	"net"
 
 	"github.com/tomocy/wataame/http/0.9/client"
 	http1_0 "github.com/tomocy/wataame/http/1.0"
+	"github.com/tomocy/wataame/ip"
 	"github.com/tomocy/wataame/tcp"
 )
 
@@ -17,6 +19,12 @@ func (c *Client) Do(ctx context.Context, r http1_0.Request) (http1_0.Response, e
 	switch r := r.(type) {
 	case *http1_0.SimpleRequest:
 		resp, err := c.doSimpleRequest(ctx, r)
+		if err != nil {
+			return nil, fmt.Errorf("failed to do: %s", err)
+		}
+		return resp, nil
+	case *http1_0.FullRequest:
+		resp, err := c.doFullRequest(ctx, r)
 		if err != nil {
 			return nil, fmt.Errorf("failed to do: %s", err)
 		}
@@ -38,4 +46,80 @@ func (c *Client) doSimpleRequest(ctx context.Context, r *http1_0.SimpleRequest) 
 	return &http1_0.SimpleResponse{
 		Response: resp,
 	}, nil
+}
+
+func (c *Client) doFullRequest(ctx context.Context, r *http1_0.FullRequest) (*http1_0.FullResponse, error) {
+	assureFullRequest(r)
+	conn, err := c.dialForFullRequest(ctx, r)
+	if err != nil {
+		return nil, fmt.Errorf("failed to do full request: %s", err)
+	}
+
+	if _, err := r.WriteTo(conn); err != nil {
+		return nil, fmt.Errorf("failed to do full request: %s", err)
+	}
+
+	respCh, errCh := c.receive(conn)
+	select {
+	case <-ctx.Done():
+		return nil, fmt.Errorf("failed to do full request: %s", ctx.Err())
+	case resp := <-respCh:
+		return resp, nil
+	case err := <-errCh:
+		return nil, fmt.Errorf("failed to do full request: %s", err)
+	}
+}
+
+func assureFullRequest(r *http1_0.FullRequest) {
+	r.RequestLine.Version = &http1_0.Version{
+		Major: 1, Minor: 0,
+	}
+}
+
+func (c *Client) dialForFullRequest(ctx context.Context, r *http1_0.FullRequest) (net.Conn, error) {
+	addr, err := ip.Addr(r.RequestLine.URI.Host).Compensate()
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial for full request: %s", err)
+	}
+
+	conn, err := c.dial(ctx, addr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial for full request: %s", err)
+	}
+
+	return conn, nil
+}
+
+func (c *Client) dial(ctx context.Context, addr string) (net.Conn, error) {
+	d := c.Dialer
+	if d == nil {
+		d = new(tcp.GoDialer)
+	}
+
+	conn, err := d.Dial(ctx, addr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial: %s", err)
+	}
+
+	return conn, nil
+}
+
+func (c *Client) receive(conn net.Conn) (<-chan *http1_0.FullResponse, <-chan error) {
+	respCh, errCh := make(chan *http1_0.FullResponse), make(chan error)
+	go func() {
+		defer func() {
+			close(respCh)
+			close(errCh)
+		}()
+
+		resp := new(http1_0.FullResponse)
+		if _, err := resp.ReadFrom(conn); err != nil {
+			errCh <- fmt.Errorf("failed to recieve: %s", err)
+			return
+		}
+
+		respCh <- resp
+	}()
+
+	return respCh, errCh
 }
--- http/1.0/client/client_test.go
+++ http/1.0/client/client_test.go
@@ -2,7 +2,10 @@ package client
 
 import (
 	"context"
+	"io/ioutil"
+	"log"
 	"net/url"
+	"strings"
 	"testing"
 
 	http0_9 "github.com/tomocy/wataame/http/0.9"
@@ -12,7 +15,8 @@ import (
 
 func TestClient_Do(t *testing.T) {
 	testers := map[string]func(t *testing.T){
-		"simple request": testClientDoSimpleRequest,
+		// "simple request": testClientDoSimpleRequest,
+		"full request": testClientDoFullRequest,
 	}
 
 	for name, tester := range testers {
@@ -51,5 +55,68 @@ func testClientDoSimpleRequest(t *testing.T) {
 	}
 	if string(actual.Response) != expected {
 		t.Errorf("unexpected Response from (*Client).Do: got %s, expect %s\n", string(actual.Response), expected)
+	}
+}
+
+func testClientDoFullRequest(t *testing.T) {
+	addr, path := ":1234", "/index.html"
+	uri, _ := url.Parse("http://localhost" + addr + path)
+	var client Client
+	serv := &server.Server{
+		Addr: addr,
+		Handler: server.HandlerFunc{
+			FullHandlerFunc: func(res *http1_0.FullResponse, req *http1_0.FullRequest) {
+				if req.RequestLine.Method != http1_0.MethodPost || req.RequestLine.URI.Path != path {
+					return
+				}
+				uas, ok := req.Header["User-Agent"]
+				if !ok {
+					return
+				}
+				if uas[0] != "wataame" {
+					return
+				}
+				body, err := ioutil.ReadAll(req.Body)
+				if err != nil {
+					return
+				}
+				if string(body) != "foo=bar" {
+					return
+				}
+
+				if res.StatusLine == nil {
+					res.StatusLine = new(http1_0.StatusLine)
+				}
+				res.StatusLine.Status = &http1_0.Status{
+					Code: 201, Phrase: "Created",
+				}
+			},
+		},
+	}
+	go serv.ListenAndServe()
+
+	input := &http1_0.FullRequest{
+		RequestLine: &http1_0.RequestLine{
+			Method: http1_0.MethodPost, URI: uri,
+		},
+		Header: http1_0.Header{
+			"User-Agent": []string{"wataame"},
+		},
+		Body: ioutil.NopCloser(strings.NewReader("foo=bar")),
+	}
+	expected := "HTTP/1.0 201 Created\n\n"
+
+	resp, err := client.Do(context.Background(), input)
+	if err != nil {
+		t.Fatalf("unexpected error from (*Client).Do: got %s, expect nil\n", err)
+	}
+	actual, ok := resp.(*http1_0.FullResponse)
+	if !ok {
+		t.Fatalf("unexpected type of Response from (*Clinet): got %T, expect *SimpleResponse\n", resp)
+	}
+	log.Println(1111111111111)
+	if actual.String() != expected {
+		log.Println(11111111111111)
+		t.Errorf("unexpected Response from (*Client).Do: got %q, expect %q\n", actual, expected)
 	}
 }
