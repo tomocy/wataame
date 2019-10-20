57cfad9b58131fe73b8ad6af7ba5c8bd60c43e1e
tomocy <tomocy.dev@gmail.com>
2019-10-20 22:53:10 +0900

    fix TestServerClient_SimpleRequest
    to let HTTP0.9 client do requests to HTTP1.0 server

--- http/1.0/server_client_test.go
+++ http/1.0/server_client_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	http0_9 "github.com/tomocy/wataame/http/0.9"
+	client0_9 "github.com/tomocy/wataame/http/0.9/client"
 	http1_0 "github.com/tomocy/wataame/http/1.0"
 	"github.com/tomocy/wataame/http/1.0/client"
 	"github.com/tomocy/wataame/http/1.0/server"
@@ -16,7 +17,7 @@ import (
 func TestServerClient_SimpleRequest(t *testing.T) {
 	addr := ":1234"
 	uri, _ := url.Parse("http://localhost" + addr + "/index.html")
-	var client client.Client
+	var client client0_9.Client
 	serv := &server.Server{
 		Addr: addr,
 		Handler: server.HandlerFunc{
@@ -29,23 +30,17 @@ func TestServerClient_SimpleRequest(t *testing.T) {
 	defer cancel()
 	go serv.ListenAndServe(ctx)
 
-	input := &http1_0.SimpleRequest{
-		Request: http0_9.Request{
-			Method: http1_0.MethodGet, URI: uri,
-		},
+	input := &http0_9.Request{
+		Method: http1_0.MethodGet, URI: uri,
 	}
 	expected := "GET /index.html\n"
 
-	resp, err := client.Do(context.Background(), input)
+	actual, err := client.Do(context.Background(), input)
 	if err != nil {
 		t.Fatalf("unexpected error from (*Client).Do: got %s, expect nil\n", err)
 	}
-	actual, ok := resp.(*http1_0.SimpleResponse)
-	if !ok {
-		t.Fatalf("unexpected type of Response from (*Clinet): got %T, expect *SimpleResponse\n", resp)
-	}
-	if string(actual.Response) != expected {
-		t.Errorf("unexpected Response from (*Client).Do: got %s, expect %s\n", string(actual.Response), expected)
+	if actual.String() != expected {
+		t.Errorf("unexpected Response from (*Client).Do: got %s, expect %s\n", actual, expected)
 	}
 }
 
