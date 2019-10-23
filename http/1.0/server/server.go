4526f5f249774f5875dab999f73c77a1233c1013
tomocy <tomocy.dev@gmail.com>
2019-10-23 16:17:22 +0900

    modify (*Server).accept
    for Server to recieve context and close its listener

--- http/1.0/server/server.go
+++ http/1.0/server/server.go
@@ -47,29 +47,32 @@ func (s *Server) listen() error {
 }
 
 func (s *Server) Serve(ctx context.Context) error {
-	connCh, errCh := s.accept()
+	connCh, errCh := s.accept(ctx)
 	for {
 		select {
-		case <-ctx.Done():
-			err := ctx.Err()
-			if err == context.Canceled {
-				return nil
-			}
-			return fmt.Errorf("failed to serve: %s", err)
 		case conn := <-connCh:
 			go s.handle(conn)
 		case err := <-errCh:
+			if err == context.Canceled {
+				return nil
+			}
 			return fmt.Errorf("failed to serve: %s", err)
 		}
 	}
 }
 
-func (s *Server) accept() (<-chan net.Conn, <-chan error) {
+func (s *Server) accept(ctx context.Context) (<-chan net.Conn, <-chan error) {
 	connCh, errCh := make(chan net.Conn), make(chan error)
 	go func() {
 		defer func() {
 			close(connCh)
 			close(errCh)
+		}()
+		defer s.Listener.Close()
+
+		go func() {
+			<-ctx.Done()
+			errCh <- ctx.Err()
 		}()
 
 		for {
