c3777d6c326082cb32c769b40f496866c254929a
tomocy <tomocy.dev@gmail.com>
2019-10-23 18:22:56 +0900

    let GoListener implement Listener

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -15,6 +15,38 @@ type GoListener struct {
 	net.TCPListener
 }
 
+func (l *GoListener) Accept(ctx context.Context) (net.Conn, error) {
+	connCh, errCh := l.acceptAsnycly()
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case conn := <-connCh:
+		return conn, nil
+	case err := <-errCh:
+		return nil, err
+	}
+}
+
+func (l *GoListener) acceptAsnycly() (<-chan net.Conn, <-chan error) {
+	connCh, errCh := make(chan net.Conn), make(chan error)
+	go func() {
+		defer func() {
+			close(connCh)
+			close(errCh)
+		}()
+
+		conn, err := l.TCPListener.Accept()
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		connCh <- conn
+	}()
+
+	return connCh, errCh
+}
+
 type Dialer interface {
 	Dial(ctx context.Context, addr string) (net.Conn, error)
 }
